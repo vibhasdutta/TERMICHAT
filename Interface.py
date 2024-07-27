569b81569ccd4fd9095bb84a4e47def38ece8491
Vibhas Dutta <113254581+vibhasdutta@users.noreply.github.com>
2024-07-27 13:25:43 +0530

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -1,11 +1,8 @@
-import dotenv
-import os
 import time
-dotenv.load_dotenv()
 import subprocess
 from Server import start
 import socket
-
+import json
 
 
 def client_run():
@@ -17,71 +14,68 @@ def client_run():
     termichat_dir = base_dir / 'Client.py'
     subprocess.Popen(['start', 'cmd', '/k', 'python',termichat_dir], shell=True)
 
+
 if __name__ == '__main__':
-
-    if os.getenv('user_name')=='your username'  or os.getenv('SERVER_IP')=='your ip or server ip' or os.getenv('PORT')=='your port':
-        print("Please set the settings first!")
-        time.sleep(2)
-        print("Enter the following details:")
-        IP_Address = input("Enter the IP Address:")
-        PORT = int(input("Enter the PORT:"))
-        Prefix = input("Enter the Prefix:")
-        UserName = input("Enter the UserName:")
-        os.putenv('USER_NAME',UserName)
-        os.putenv('Prefix',Prefix)
-        os.putenv('PORT',str(PORT))
-        os.putenv('SERVER_IP',IP_Address)
-
-    try:
-        Prefix = os.getenv('Prefix')
-        UserName = os.getenv('USER_NAME')
-        PORT = int(os.getenv('PORT'))
-        IP_Address = os.getenv('SERVER_IP')
-        ADDR=(IP_Address,PORT)
-        server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        server.bind(ADDR)
-    except Exception:
-        print("The IP Address or PORT is invalid!")
-        exit()
-
+    
+    with open('config.json') as f:
+        data = json.load(f)
 
     print("Welcome to the TERMICHAT!")
 
-    print("Do you want to Host or Connect the SERVER?[Y/N]")
+    print("Do you want to Host or Connect the SERVER?[H/C]")
     while True:
         choice=input("Enter your choice:")
-        if choice.lower()=='y':
-            IP_Address = socket.gethostbyname(socket.gethostname())
-            os.putenv('HOSTING_ON','True')
+        if choice.lower()=='h':
+            with open('config.json','w') as f:
+                data['SERVER_IP'] = socket.gethostbyname(socket.gethostname())
+                data['PORT'] = int(input("Enter the Server PORT:"))
+                json.dump(data,f)
             break;
-        elif choice.lower()=='n':
-            pass
+        elif choice.lower()=='c':
+            with open('config.json','w') as f:
+                data['SERVER_IP'] = input("Enter the Server IP:")
+                data['PORT'] = int(input("Enter the Server PORT:"))
+                json.dump(data,f)
+            break;
         else:
             print("Invalid Choice!")
         
-
     while True:
         try:
-            print(f"\nEnter {Prefix}start_server : to start the Server \n{Prefix}settings : to set server ip,port,prefix and username \n{Prefix}exit : to exit the chat\n")
+
+            with open('config.json') as f:
+                    data = json.load(f)
+
+            Prefix = data['PREFIX']
+            UserName = data['USER_NAME']
+            PORT = data['PORT']
+            IP_Address = data['SERVER_IP']
+
+
+            print(f"\nEnter {Prefix}start_server : to start the Server \n{Prefix}settings : to set prefix and username \n{Prefix}exit : to exit the chat\n")
             choice=input("Enter your choice:")
             
             if all (character in choice for character in [Prefix,'start_server']):
+                ADDR=(IP_Address,PORT)
+                server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+                server.bind(ADDR)
 
-                start(UserName,Prefix,server,ADDR)
+                start(UserName,Prefix,server,ADDR,IP_Address,PORT)
                 time.sleep(2)
             
             elif all (character in choice for character in [Prefix,'settings']):
                     
                     print("Enter the following details:")
-                    IP_Address = input("Enter the IP Address:")
-                    PORT = int(input("Enter the PORT:"))
-                    Prefix = input("Enter the Prefix:")
-                    UserName = input("Enter the UserName:")
-                    os.putenv('USER_NAME',UserName)
-                    os.putenv('Prefix',Prefix)
-                    os.putenv('PORT',str(PORT))
-                    os.putenv('SERVER_IP',IP_Address)
-
+                    IP_Address=input("Enter the IP Address:")
+                    PORT=int(input("Enter the PORT:"))
+                    Prefix=input("Enter the Prefix:")
+                    UserName=input("Enter the UserName:")
+                    with open('config.json','w') as f:
+                        data['PREFIX'] = Prefix
+                        data['USER_NAME'] = UserName
+                        json.dump(data,f)
+                    print("Settings Updated!")
+                    
             elif all (character in choice for character in [Prefix,'exit']):
                 
                 print("Exiting...")
